84c99a4298de593da99e8a1cd3444c10a83f6452
BertalanBalazs <bertalan10@gmail.com>
2019-07-22 14:55:03 +0200

    init base test

---
+++ src/test/java/com/codecool/lpt/BaseTest.java
@@ -0,0 +1,24 @@
+package com.codecool.lpt;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+abstract class BaseTest {
+
+    WebDriver driver;
+
+    @BeforeEach
+    void setUp() {
+        System.setProperty("webdriver.chrome.driver", System.getenv("webdriverPath"));
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        driver.close();
+    }
+}
